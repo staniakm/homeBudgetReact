1f77b2be7fbcd4c1c9d4e19dc3724d8b46ddcdfe
Mariusz Staniak <mariusz.staniak@gmail.com>
2019-03-19 20:54:37 +0100

    charts

--- src/SPA/Charts.js
+++ src/SPA/Charts.js
@@ -6,27 +6,24 @@ import { Chart } from "react-google-charts";
 class Charts extends React.Component {
 
     state = {
-        isLoaded:false
+        isLoaded: false
     }
-    columns = 
+    columns =
         [["Task", "Hours per Day"]]
-    ;
+        ;
     charities = [...this.columns];
 
-
-
-componentDidMount(){
-    fetch("http://localhost:8080/api/shopping/info")
-    .then(response => response.json())
-    .then(data => (data.map(obj => Object.values(obj)).map(item => {
-        return this.charities.push(item)
-    }) ))
-    .then(this.setState({
-        data: this.charities
-    })).then(this.setState({
-        isLoaded:true
-    }))
-}
+    componentDidMount() {
+        fetch("http://localhost:8080/api/shopping/info")
+            .then(response => response.json())
+            .then(data => (data.map(obj => Object.values(obj)).map(item => {
+                return this.charities.push(item)
+            })))
+            .then(this.setState({
+                data: this.charities,
+                isLoaded: true
+            }))
+    }
 
     options = {
         title: "My Daily Activities",
@@ -35,19 +32,17 @@ componentDidMount(){
     };
 
     render() {
-        // console.log(this.state.isLoaded)
-        // console.log(this.state.data)
         return (
             <div>
                 <h1>Charts</h1>
-                <Chart
-                    chartType="PieChart"
-                    width="100%"
-                    height="400px"
-                    data={this.state.data}
-                    options={this.options}
-                    
-                />
+                {this.state.isLoaded &&
+                    <Chart
+                        chartType="PieChart"
+                        width="100%"
+                        height="400px"
+                        data={this.state.data}
+                        options={this.options}
+                    />}
             </div>
         )
     }
