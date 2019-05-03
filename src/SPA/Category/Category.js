e21b06621ffbe6913d148fc17be99b180b480234
Mariusz Staniak <mariusz.staniak@gmail.com>
2019-05-03 19:50:54 +0200

    add month switch for category list

--- src/SPA/Category/Category.js
+++ src/SPA/Category/Category.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import * as url from '../../Navigation/ulrs'
 import sorter from '../../Util/Sort'
-import { Table } from 'reactstrap';
-import axios from 'axios'
+import { Table, Button } from 'reactstrap';
+import axios from 'axios';
+import {connect} from 'react-redux'
+import { setMonth} from '../../Action';
+
 class Category extends Component {
 
     state = {
         isLoaded: false,
         data: [],
-        ascOrder:true
+        ascOrder: true
     }
 
-    sortBy(key){
-        const {data, ascOrder} = this.state
+    sortBy(key) {
+        const { data, ascOrder } = this.state
         const sorted = sorter(key, data, ascOrder)
         this.setState({
             data: sorted,
@@ -24,8 +27,8 @@ class Category extends Component {
         <thead>
             <tr className="oneRow sortable">
                 <th scope="col" onClick={() => this.sortBy('name')}>Nazwa</th>
-                <th scope="col" onClick={() => this.sortBy('monthSummary')}>Koszt w danym miesiącu (zł)</th>
-                <th scope="col" onClick={() => this.sortBy('yearSummary')}>Koszt w danym roku (zł)</th>
+                <th scope="col" onClick={() => this.sortBy('monthSummary')}>Koszt w {this.props.monthValue} (zł)</th>
+                <th scope="col" onClick={() => this.sortBy('yearSummary')}>Koszt w {this.props.monthValue.substring(0, 4)} roku (zł)</th>
             </tr>
         </thead>
     );
@@ -43,30 +46,59 @@ class Category extends Component {
         </tbody>
     );
 
+    NavigationTab = () => (
+        <div className="rowC">
+            <Button outline color="success" onClick={() => this.changeMonth(-1)}>Poprzedni miesiąc</Button>
+            <Button outline color="success" onClick={() => this.changeMonth(0)}>Obecny miesiąc</Button>
+            <Button outline color="success" onClick={() => this.changeMonth(1)}>Następny miesiąc</Button>
+        </div>
+    )
+
+    changeMonth = (value) => {
+        const month = (value === 0) ? 0 : this.props.month + value
+        this.props.setMonth(month);
+        this.loadData(month)
+    }
+
     onCategoryClick = (item) => {
         this.props.history.push(`/category/${item}`)
     }
 
     componentDidMount() {
-        axios.get(url.CATEGORY)
-            .then(response => 
+        this.loadData(0);
+    }
+
+    loadData = month => {
+        axios.get(url.CATEGORY+`?month=${month}`)
+            .then(response =>
                 this.setState({
-                data: response.data,
-                isLoaded: true
-            }))
+                    data: response.data,
+                    isLoaded: true
+                }))
     }
 
     render() {
         return (
             <div>
-                <h1>Lista kategorii</h1>
+                <this.NavigationTab />
                 <Table striped>
                     <this.TableHeader />
                     <this.TableBody />
-                    </Table>
+                </Table>
             </div>
         )
     }
 }
 
-export default Category
+const mapStateToProps = (state) => {
+    return {
+        month: state.monthReducer.month,
+        monthValue: state.monthReducer.monthValue,
+    }
+};
+
+const mapDispatchToProps = ({
+    setMonth,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps) (Category)
